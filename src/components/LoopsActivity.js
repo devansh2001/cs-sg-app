540d99a59f139e36bddac5c49146ca13e1bb73c4
PRERNARAVI <prernaravi2k@gmail.com>
2020-12-02 01:19:55 -0500

    Added original pattern component

--- src/components/LoopsActivity.js
+++ src/components/LoopsActivity.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 
-
+const OriginalPattern  = () => {
+    let boxes = [];
+    for (let i = 0; i < 5; i++) {    
+        for (let j = 0; j <= i; j++) {
+            boxes.push("* ");
+        }
+        boxes.push(<br/>);
+    }
+    console.log("entered");
+    return (
+        <div >  
+            {boxes} 
+        </div>
+    )
+}
 class LoopsActivity extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            originalPattern: '',
-            userPattern: ''
+            userPattern: '',
         }
     }
     render() {
-        let boxes = [];
-        for (let i = 0; i < 4; i++) {    
-            for (let j = 0; j <= i; j++) {
-                boxes.push("*");
-            }
-            boxes.push(<br/>);
-        }
+        // let boxes = [];
+        // for (let i = 0; i < 5; i++) {    
+        //     for (let j = 0; j <= i; j++) {
+        //         boxes.push("* ");
+        //     }
+        //     boxes.push(<br/>);
+        // }
         return (
             <Container style={{textAlign: 'center'}}>
                 <h2>Loops Activity</h2>
@@ -31,9 +44,9 @@ class LoopsActivity extends Component {
                             <div>
                                 <h5>Given below on the left hand side is a pattern of stars obtained from
                                     using nested for loops. The first for loop specifies the row number 
-                                    for the pattern which in this case runs from 1 to 4. The second for loop 
+                                    for the pattern which in this case runs from 1 to 5. The second for loop 
                                     specifies the number of stars needed on each row of the pattern, which again
-                                    ranges from 1 to 4. 
+                                    ranges from 1 to 5. 
                                 </h5>
                                 <br></br>
                                 <h5>Wite a nested for loop to display the same pattern on the right hand side.
@@ -48,8 +61,12 @@ class LoopsActivity extends Component {
                         <Col>
                             <Container>
                                 <h5>Correct Pattern</h5>
-                                <Container style={{alignContent: "flex-start"}}>
-                                    {boxes}
+                                <Container >
+                                    <Col>
+                                    <Row className="star-pattern">
+                                        <OriginalPattern/>      
+                                    </Row>
+                                    </Col>
                                 </Container>
                             </Container>
                         </Col>
