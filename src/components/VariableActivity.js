3fd59a59ae056d2777169041facc6bb30d2dc092
Jessica <jessicambishop103@gmail.com>
2020-12-07 17:28:54 -0500

    comment out code for variable activity

--- src/components/VariableActivity.js
+++ src/components/VariableActivity.js
@@ -18,40 +18,53 @@ class VariableActivity extends Component {
         ]
     }
 
+    /*
+    * Step 1:
+    * Add a variable in the state that is called "name"
+    * Set the variable to have a default of ""
+    * Look at the slides if you forget how to declare a state variable!
+    */
     state = {
         textBoxValue: "",
-        name: "",
-        phrase: ""
+        phrase: "",
+        // add the new variable here
     };
     
+    /*
+    * Step 2:
+    * Change the value of the state variable "name" to this.state.textBoxValue
+    * this.state.textBoxValue is the variable that holds the value you typed into thet textbox on the page
+    * This exercise also has you practicing how to call a state variable with this.state.textBoxValue!
+    * Look at the slides if you forget how to change state!
+    * 
+    * Learning note for onSubmit:
+    * This particular method is called whenever the "Go!" button is pressed
+    * What it is doing is assigning the value in the text box to your name variable
+    * This variable is accessed to be displayed for you to see in the JSX code below
+    */
     onSubmit = () => {
-        this.setState({
-            name: this.state.textBoxValue
-        })
-
+        // change the value of name here
     };
+    
+    /*
+    * Step 3:
+    * Add your name variable to the phrase passed in and then change the value of phrase to this new string
+    * Look at the slides if you forget how to change state or make a regular variable!
+    * Remember: you are able to access parameters like they are regular variables
+    */
+    getRandomPhrase = (phrase) => {
+        // create a new regular variable that takes the name variable and adds the parameter phrase to the end of it
+        // You will need to access the state variable name and the phrase parameter
+        // Remember: we can add strings together using concatenation (+)
+        // Play around with this by adding extra spaces, changing the order, etc.
 
-    // we should explain state really high level to them before we begin
-    // maybe explain state as a collection of variables that define the current values for all of the application or something idk
-    // three ways to do this -> use easiest
-    // make it an instance variable this.myName
-    // use state this.state.displayName
-    // or pass in parameter
-    // we need to add methods and params into the variable talk then
-    // also add a slide for string concat and placement
-    // and arrays and indexing if we're feelxing fancy
-    // if we don't want to add this to the slides can alwasy explain it in comments and tell them what to do
-    getRandomPhrase = () => {
-        // this line will be partially commented out and spelled out how to do
-        console.log('here')
-        let ourPhrase = this.state.name + " " + this.phrases[this.currentPhraseIndex]
+        /* Create and assign new variable here */
+        
+        // Now, you will need to change the value of the state variable phrase to the new phrase you made above
+        // You will need to use the setState({}) method to change phrase
+        // Remember how to access regular variables you made!
 
-        this.currentPhraseIndex += 1
-        this.currentPhraseIndex %= 7
-        console.log(this.currentPhraseIndex)
-        this.setState({
-            phrase: ourPhrase
-        })
+        /* Change the value of phrase to the new phrase here */
     }
 
     render() {
@@ -95,7 +108,12 @@ class VariableActivity extends Component {
             <Row className="justify-content-md-center">
                 <Button
                 className="btnFormSend"
-                onClick={this.getRandomPhrase}
+                onClick={() => {
+                    let newPhrase = this.phrases[this.currentPhraseIndex]
+                    this.currentPhraseIndex += 1
+                    this.currentPhraseIndex %= 7
+                    this.getRandomPhrase(newPhrase)
+                }}
                 >
                 Make my random phrase!
                 </Button>
